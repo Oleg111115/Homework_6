e2f168f3763b4dd9dc4f3240bb163f1fff9e6f2a
Олег <mishin415@yandex.ru>
2023-03-24 00:41:27 +0300

    random.sample

--- 6_4.py
+++ 6_4.py
@@ -2,3 +2,5 @@ import random
 
 list_comprehension = random.sample(range(1, 1415), 9)
 print("Список случайных чисел: " + str(list_comprehension))
+
+#Список случайных чисел: [755, 1404, 419, 505, 1064, 410, 706, 75, 36]
