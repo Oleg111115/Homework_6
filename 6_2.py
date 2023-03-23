6fbf1cd68743542a578291e1ecc18132d4d0eb9f
Олег <mishin415@yandex.ru>
2023-03-24 00:58:52 +0300

    combinations

--- 6_2.py
+++ 6_2.py
@@ -7,3 +7,4 @@ combinations_list.append([list[1], list[0], list[2]])
 combinations_list.append([list[2], list[0], list[1]])
 combinations_list.append([list[2], list[1], list[0]])
 print(combinations_list)
+#[[1, 2, 3], [1, 3, 2], [2, 3, 1], [2, 1, 3], [3, 1, 2], [3, 2, 1]]
